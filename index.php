faeafea6246cd5f1bf79af6864edffb50b3b0db4
Phạm Ngọc Hiếu <hieupn89@gmail.com>
2015-12-25 16:53:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-        <title></title>
-    </head>
-    <body>
-        <div>
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                        <p class="first-iloveyou"><?php the_title(); ?></p>
-                        <p><?php the_content(); ?></p>
-                        <hr> <?php endwhile;
-           else: ?>
-                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-        </div>   
-    </body>
-</html>
+/*
+Theme Name: phamngochieu
+Theme URI: http://tranvietanh.com/
+Author: Viet Anh - tranvietanh.com
+Author URI: http://tranvietanh.com
+Description: hướng dẫn tạo theme wordpress
+Version: 1.0
+*/
+*
+{ 
+  margin: auto;
+}
+body>div
+{
+    width:70%;
+}
+div>p
+{
+   text-align: center;
+   background-color: appworkspace;
+}
+p.first-iloveyou
+{
+   background-color: chartreuse; 
+}
