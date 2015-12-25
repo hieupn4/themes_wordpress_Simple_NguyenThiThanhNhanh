2781df0dd9a7871ebebd3bc0c1c99d695723e5ab
Phạm Ngọc Hiếu <hieupn89@gmail.com>
2015-12-25 10:48:32 +0800

    thử làm cái theme đơn giản xem sao

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>HEADER</h1>
+            </div>
+        </div>
+    </body>
+</html>
